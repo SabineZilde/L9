6be175584a877d18cebefcb2823d8dc1a588435d
SabineZilde <sabine.zilde@protonmail.ch>
2021-06-08 16:21:39 +0300

    Fixing index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
-// import dotenv from 'dotenv';
-// import articlesRoutes from './routes/articles.js';
-// import express from 'express';
-// import bodyParser from 'body-parser';
+import express from 'express';
 
+const server = express(); //Creating express server
 
-// dotenv.config(); //
-// const server = express(); //Creating express server
-// server.use(bodyParser.json());
-
-
-// server.use('/articles', articlesRoutes); // all the routes will start with /articles, so we can remove /articles part in file articles.js
-
-
-server.listen(process.env.PORT, () => { // starting server
-    console.log(`Express is up and running on port ${process.env.PORT}`);
-});
-import http from 'http';
-
-// defing our web server
-const server = http.createServer((req, res) => { // param = request and response
-    res.end('Hello cats and dogs!');
-});
+server.get('/', (req, res) => { //Creating request
+    res.send('Hello world!');
+})
 
 server.listen(8070, () => {
-    console.log('Server is up and running!')
+    console.log('Express is up and running!');
 });
